2b1605988a149ddc7fcd7434b154abe2993da064
Itai Birenshtok <Itai.Birenshtok@cellebrite.com>
2018-10-23 23:36:43 +0300

    refactoring

--- app.js
+++ app.js
@@ -14,45 +14,51 @@ const connection = mysql.createConnection({
     database: 'rsxx9m87051sudx7'
 });
 
-const configuration = { countries: 'USA'};
-connection.query('INSERT INTO configurations SET ?', configuration, (err, res) => {
-    if(err) throw err;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-    console.log('Last insert ID:', res.insertId);
-});
+app.get('/', (req, res) => {
+    const configuration = {countries: 'USA'};
+    connection.query('INSERT INTO configurations SET ?', configuration, (err, res) => {
+        if (err) throw err;
 
-googleMapsClient.reverseGeocode({
-    latlng : {lat:"40.714224", lng:"-73.961452"}
-}, function(err, response) {
-    if (!err) {
-        console.log(response.json.results);
-    }
-});
+        console.log('Last insert ID:', res.insertId);
+    });
 
-const client = new elasticsearch.Client({
-    host: 'https://elastic:5t8e50MT30Pwbed6g5X2zVgk@474197f539d74ae980590eb60963a9f1.eu-central-1.aws.cloud.es.io:9243',
-    log: 'trace'
-});
-
-client.search({
-    index: indexName,
-    body: {
-        query: {
-            match_all:{}
+    googleMapsClient.reverseGeocode({
+        latlng: {lat: "40.714224", lng: "-73.961452"}
+    }, function (err, response) {
+        if (!err) {
+            console.log(response.json.results);
         }
-    }
-}).then(function (resp) {
-    const hits = resp.hits.hits;
-    let response = "";
-    for (let i = 0; i < hits.length ; i++) {
-        response = response.concat("Type of event " + i + " is: " + hits[i]._source.Type + "\n")
-    }
-    app.get('/', (req, res) => res.send(response))
+    });
 
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-}, function (err) {
-    console.trace(err.message);
+    const client = new elasticsearch.Client({
+        host: 'https://elastic:5t8e50MT30Pwbed6g5X2zVgk@474197f539d74ae980590eb60963a9f1.eu-central-1.aws.cloud.es.io:9243',
+        log: 'trace'
+    });
+
+    client.search({
+        index: indexName,
+        body: {
+            query: {
+                match_all: {}
+            }
+        }
+    }).then(function (resp) {
+        const hits = resp.hits.hits;
+        let response = "";
+        for (let i = 0; i < hits.length; i++) {
+            response = response.concat("Type of event " + i + " is: " + hits[i]._source.Type + "\n")
+        }
+        return res.send(response);
+    }, function (err) {
+        return res.send(err.message);
+    });
 });
+
+const country_list = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Cayman Islands", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kuwait", "Kyrgyz Republic", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Mauritania", "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Satellite", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "St. Lucia", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
+app.get('/optional-countries', (req, res) => res.send(country_list))
+
 
 
 
