e5473c1b3ec70c10f438c97e957048113a688364
Itai Birenshtok <Itai.Birenshtok@cellebrite.com>
2018-10-23 21:33:15 +0300

    small refactoring

--- app.js
+++ app.js
@@ -4,17 +4,11 @@ const mysql = require('mysql');
 const app = express()
 const port = 4433
 const indexName = 'a11e693f-d6d2-11e8-b283-382c4ab4a532_discoveryindexkind'
-
-
 const connection = mysql.createConnection({
     host: 'u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user: 'hf8t3p2wowg351hr',
     password: 's4fp8u046l30aloj',
     database: 'rsxx9m87051sudx7'
-});
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected!');
 });
 
 const configuration = { countries: 'USA'};
