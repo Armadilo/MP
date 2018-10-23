86be9708511932f2e5b88d457ef4262bd0d47299
itai <itai.birenshtok@cellebrite.com>
2018-10-23 15:50:15 +0300

    route to new elastic

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express')
 var elasticsearch = require('elasticsearch');
 const app = express()
 const port = 4433
-const indexName = '90e233e7-d69c-11e8-b283-382c4ab4a532_discoveryindexkind'
+const indexName = '16ed9140-d6c0-11e8-b283-382c4ab4a532_discoveryindexkind'
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 var client = new elasticsearch.Client({
-    host: 'https://hzdaaj9c7q:ckfepucewy@fig-5830594.us-east-1.bonsaisearch.net',
+    host: 'https://elastic:5t8e50MT30Pwbed6g5X2zVgk@474197f539d74ae980590eb60963a9f1.eu-central-1.aws.cloud.es.io:9243',
     log: 'trace'
 });
 
