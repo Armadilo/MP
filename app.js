e2549c7f5c01bdbfb0e4bfc4774549ffc8184eae
Itai Birenshtok <Itai.Birenshtok@cellebrite.com>
2018-10-23 21:31:33 +0300

    add mysql integration

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express')
 var elasticsearch = require('elasticsearch');
+const mysql = require('mysql');
 const app = express()
 const port = 4433
 const indexName = 'a11e693f-d6d2-11e8-b283-382c4ab4a532_discoveryindexkind'
+
+
+const connection = mysql.createConnection({
+    host: 'u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'hf8t3p2wowg351hr',
+    password: 's4fp8u046l30aloj',
+    database: 'rsxx9m87051sudx7'
+});
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+});
+
+const configuration = { countries: 'USA'};
+connection.query('INSERT INTO configurations SET ?', configuration, (err, res) => {
+    if(err) throw err;
+
+    console.log('Last insert ID:', res.insertId);
+});
 
 var client = new elasticsearch.Client({
     host: 'https://elastic:5t8e50MT30Pwbed6g5X2zVgk@474197f539d74ae980590eb60963a9f1.eu-central-1.aws.cloud.es.io:9243',
