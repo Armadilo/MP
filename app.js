d317ae18bb9b444a539e3f6760bb591c94b0bde5
itai <itai.birenshtok@cellebrite.com>
2018-10-23 15:52:15 +0300

    fix

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var elasticsearch = require('elasticsearch');
 const app = express()
 const port = 4433
 const indexName = '16ed9140-d6c0-11e8-b283-382c4ab4a532_discoveryindexkind'
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 var client = new elasticsearch.Client({
     host: 'https://elastic:5t8e50MT30Pwbed6g5X2zVgk@474197f539d74ae980590eb60963a9f1.eu-central-1.aws.cloud.es.io:9243',
@@ -21,6 +18,9 @@ client.search({
     }
 }).then(function (resp) {
     var hits = resp.hits.hits;
+    app.get('/', (req, res) => res.send('Hello World!'))
+
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 }, function (err) {
     console.trace(err.message);
 });
